d889b60ceb7034e5b94e0b3c93e79189999c8870
DeepanshUnravel <deepansh@gounravel.com>
2025-09-07 23:51:27 +0530

    Refactor GalleryComponent layout and styles for improved responsiveness and visual consistency

--- client/src/Components/GalleryComponent.jsx
+++ client/src/Components/GalleryComponent.jsx
@@ -2,75 +2,75 @@
 import "../Styles/Gallery.css"
 
 const GalleryComponent = () => {
-  return (
-      <section className="mainContainer">
+    return (
+        <section className="mainContainer">
 
-          <div className="layoutDivision pr1">
-              <video src=" https://d1tf573zhz3zzy.cloudfront.net/data/content/videos/CantoTranscoded/720p/YOUR+PASSPORT/2hklt3vcbt0pvcgeo093i0ps6f.mp4 " className="largePortrait br4" autoPlay muted playsInline loop controls={false} />
-          </div>
+            <div className="layoutDivision pr1">
+                <video src=" https://d1tf573zhz3zzy.cloudfront.net/data/content/videos/CantoTranscoded/720p/YOUR+PASSPORT/2hklt3vcbt0pvcgeo093i0ps6f.mp4 " className="largePortrait br4" autoPlay muted playsInline loop controls={false} />
+            </div>
 
-          <div className="layoutDivision pr1 flexColumn">
+            <div className="layoutDivision pr1 flexColumn">
 
-              <div className="layoutPortraitDivision pb1">
+                <div className="layoutPortraitDivision pb1">
 
-                  <div className="quarter pr1">
-                      <img src="https://d1tf573zhz3zzy.cloudfront.net/data/content/images/itemImages/HOTEL/portrait/7095934f-61d6-40fd-93a2-89993605a8ae_twox_256x368.jpg" alt="" className="br4" />
-                  </div>
+                    <div className="quarter pr1">
+                        <img src="https://d1tf573zhz3zzy.cloudfront.net/data/content/images/itemImages/HOTEL/portrait/7095934f-61d6-40fd-93a2-89993605a8ae_twox_256x368.jpg" alt="" className="br4" />
+                    </div>
 
-                  <div className="quarter pl1">
-                      <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AXQCQNTNCno2Db952a3WurvmzqwPBza8RgOdiLlEO5xTY256TL-mYEn2qhtvh8uLTOsuaCW_iHn7Ls7Rara56LtKOfWCSt1ALyM1cgV3iOPCNOO6FOz96jfoLpUUtSc1zKMXXkdM2p7Be_nuAxoaP8N-AbvFZ3KeF_HzDzoOkoVz5BZkgtlqRkXir2nt4y8g14NzKmVySEek7OnmtsNxVzaoWPmKglbHjr70-oDdhTzT0clOCqQfFUUaCR50QXMDUXMPbTMmpsen5Mgh9toHCyL2c2hLfe9RM9cbf250znhyo9AD203CN8JPUc0jW-xqJ9GlNue-2i3tknk&key=AIzaSyBCCAi9CmRJOAlx3QeiPh637GOIh2YGdpM" alt="" className="br4" />
-                  </div>
+                    <div className="quarter pl1">
+                        <img src="https://images.unsplash.com/photo-1757151380289-a7e0a1f2a39d?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="br4" />
+                    </div>
 
-              </div>
+                </div>
 
-              <div className="layoutLandscapeDivision">
-                  <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AXQCQNSOiM5Yg0WtdSORi_nqEsBmwMS2wuYD3vJlVJnx16fV3rjiWEFXf6JHhIE0cnG8vLS--9Lul80rG5R84pzlc6ZxMmf6yb2dxhfSoNEjZE4WyOej8KqO4qv_Us9NGe8pqkoUzJiQgnI7t55fdVZSaU7Xni-8HxyL1I6qvThqDm6e5CNUXKDEpph_BOiO0qnpxCiqu32jtBH1579_GjkzaVxG3ZH5EHfbJsz6H56nFgs9n-88em_EKI4sdEXPHWClnDqYXiWVbOv2CeBRBGFNM8Pf1cPGXHDDD2KvYAUAF38UkeAq70oWCey_FPsEjcQp9BEx6ruc77A&key=AIzaSyBCCAi9CmRJOAlx3QeiPh637GOIh2YGdpM" alt="" className="br4" />
-              </div>
+                <div className="layoutLandscapeDivision">
+                    <img src="https://plus.unsplash.com/premium_photo-1673697239981-389164b7b87f?q=80&w=1444&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="br4" />
+                </div>
 
-          </div>
+            </div>
 
-          <div className="layoutDivision pr1 flexColumn">
+            <div className="layoutDivision pr1 flexColumn">
 
-              <div className="layoutLandscapeDivision pb1">
-                  <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AXQCQNSGn8i6z7IpK7Sk9iYrKYtdlHJA-nyAQ79hCs9q1Xx7urNWOcL_h628mN-QhPkKg7O4-Wv8mTXOedI3d47uZlBZvkQspubZtP5hAmzb4aNqgR6B1LRfX3ZXMMJn22AkDiT6xuGvxQrFnpdqffqUXemGPTm0fCHOaMMa1YkpDK51LwHSPzj9ds8CA3OWs7IYvu83kZLIqoJHG7db09qk1opPx__O4Cx7kTo5nDXT-juHHpjQS-WHz9y-C9tnjRANl3UDHnkj0ze49maI8MOKuzObZCmtEYFLLM3EjshyMATm5fzLvx_I1mohaVx6e60sY_zLHZVNj9Q&key=AIzaSyBCCAi9CmRJOAlx3QeiPh637GOIh2YGdpM" alt="" className="br4" />
-              </div>
+                <div className="layoutLandscapeDivision pb1">
+                    <img src="https://plus.unsplash.com/premium_photo-1673697239981-389164b7b87f?q=80&w=1444&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="br4" />
+                </div>
 
-              <div className="layoutPortraitDivision">
+                <div className="layoutPortraitDivision">
 
-                  <div className="quarter pr1">
-                      <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AXQCQNSszCbis591sJf-xZ9zPz_Z1gQCJjUvRnf6XWBOCnbQ_KIE9Ksl5OHxaTMqyJe5B3eB7F7_VChzdmOiNQi3VZ-snbF1SIC9J57DYhhUyb6DpiPMsa6T4jzvdezcC9-wWNm0El7TXUkCTRjxWh4UUtkGS7JaTEJfZJd1mozh02vuwQTUNtP0hJlYXXE-z42L1XKa2WaAL3ws8irCmt8MDqN_V6Rc7HFaJxjlduqQUE2xOOmUfgYd7ysmo0rdgyfiEcB2COXsTp_ukI-YLE6stMZqxp7clI_AEIf5-5rlHNqs-ewKFne9_YC_F0Yz1Wjw8oemnWwwbNc&key=AIzaSyBCCAi9CmRJOAlx3QeiPh637GOIh2YGdpM" alt="" className="br4" />
-                  </div>
+                    <div className="quarter pr1">
+                        <img src="https://images.unsplash.com/photo-1617118069801-08850f78ad96?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="br4" />
+                    </div>
 
-                  <div className="quarter pl1">
-                      <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AXQCQNSZ1kv2yWlOH8s_EiTd9px4ApnRo1T-O2k58899Ma88g9YYlXtMNU6cn6KN04Eponb6jbI4T1IgRMcYV6uxY2jWRfbrb-cbgRRltD_-QYLMsM2w7HC6tkSkfUUjOsjmir9KC9e_xKM0XhRmjNyipQm6Qn7ibe34KBQHy1CT8LZBFFC_pPO3rIDghxnakPTnZtbVFSClIe0n50UodJU3ueUxaPtiKZermx-76UHdpMrzsthHTqR5FHzL1BSv6bdSnB-CN6yhCVimWip0UJzVjDKRs_2EQIqjgBJqyCBPisO2e4wEM0PMdetShhjrQ1ld4_eh1h7cVZ8&key=AIzaSyBCCAi9CmRJOAlx3QeiPh637GOIh2YGdpM" alt="" className="br4" />
-                  </div>
+                    <div className="quarter pl1">
+                        <img src="https://images.unsplash.com/photo-1659743724651-00e46aa6f805?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8" alt="" className="br4" />
+                    </div>
 
-              </div>
+                </div>
 
-          </div>
+            </div>
 
-          <div className="layoutDivision  flexColumn">
+            <div className="layoutDivision  flexColumn">
 
-              <div className="layoutPortraitDivision pb1">
+                <div className="layoutPortraitDivision pb1">
 
-                  <div className="quarter pr1">
-                      <img src="https://d1tf573zhz3zzy.cloudfront.net/data/content/images/itemImages/HOTEL/portrait/abc77611-bdd3-4948-82f9-be8270700b0a_twox_256x368.jpg" alt="" className="br4" />
-                  </div>
+                    <div className="quarter pr1">
+                        <img src="https://images.unsplash.com/photo-1659743724651-00e46aa6f805?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEwfHx8ZW58MHx8fHx8" alt="" className="br4" />
+                    </div>
 
-                  <div className="quarter">
-                      <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AXQCQNQYH5DE0I12-T-Xg0PurFmjd-m4UgXKJfItja2YqjC1FcpJG1Gll6GI075g23mkYSphJPvzW9KZoslwMvfu1E3-8-yZXxt5K71I_4IlwnesnKoxOmZKsVz6bLOVd5-vmJy_FZwNMCAASqTbxyjBzuoGuQ3U_vqY-PhsvKxpP8mxjxBOhkrWXkQds6ecrghTG0bcsK_tRvcC_7kkC4JIFAoGj5hRz7TbBozYkb5qrbyjeDUXTgSwz1oRPTsfSQTvCiLGroF9V92BAgP_7BSSpRc7LO2UGEtb-RkUaZ1Vx9Tnhd7mLacHLEK8RREDzFDC7haZFgNJ97s&key=AIzaSyBCCAi9CmRJOAlx3QeiPh637GOIh2YGdpM" alt="" className="br4" />
-                  </div>
+                    <div className="quarter">
+                        <img src="https://images.unsplash.com/photo-1617118069801-08850f78ad96?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="br4" />
+                    </div>
 
-              </div>
+                </div>
 
-              <div className="layoutLandscapeDivision">
-                  <img src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=AXQCQNR3ngNSr1u0k4Y6bLBivhUGnfeuvfjEPbg2mtUc1tgEbvXh0bxAagz4r-f5tbcJLBJXfkZxaCOS9VxbgfIWkDqvgAz9NpG9gzVe28zfrpvt3tROSI5s_jezbuwynEq2-sa7PeIf95BPQVO1KlFPXGWtRLpEY46O-4y4M8fta6YJtxz44UvCVd53tzBh5QJ9nOMLjInOmMrnOpRMzlIJWnFlYF7RjYkg0H29ThSc0iAlWhgxZ5KHsuXidQIToksoOIiS2LSlSCaX39Vhik_XDGsc9W4vBWjn7vPzx7BFMii9i5csMOlK7rgLHMmcNo-5ZRmPt5eMm38&key=AIzaSyBCCAi9CmRJOAlx3QeiPh637GOIh2YGdpM" alt="" className="br4" />
-              </div>
+                <div className="layoutLandscapeDivision">
+                    <img src="https://plus.unsplash.com/premium_photo-1673697239981-389164b7b87f?q=80&w=1444&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="br4" />
+                </div>
 
-          </div>
+            </div>
 
-      </section>
-  )
+        </section>
+    )
 }
 
 export default GalleryComponent
