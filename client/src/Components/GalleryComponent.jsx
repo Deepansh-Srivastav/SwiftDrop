bd695def17158e89f33c5d006b02f7c7f3d26fb5
DeepanshUnravel <deepansh@gounravel.com>
2025-09-08 02:22:58 +0530

    j

--- client/src/Components/GalleryComponent.jsx
+++ client/src/Components/GalleryComponent.jsx
@@ -1,12 +1,15 @@
 
 import "../Styles/Gallery.css"
+import {
+    projectImages
+} from "../Assets/Assets.js"
 
 const GalleryComponent = () => {
     return (
         <section className="mainContainer">
 
             <div className="layoutDivision pr1">
-                <img src="https://lh3.googleusercontent.com/rd-gg-dl/AJfQ9KRknq7kJON9FF_5umWI2c_Uw2jKoRwCHXkY61k8CzqcL3yZS_elxHCCRWNNTPJYwwgnqaA0RGUeyW1R1gQAS88rzDFbcp5tGCbMdiyXlqZh-OIO0BQFe6LFxNNMS-9V5JJRb7IfRh20UWDytRR2jDGOU331cYy_JxAEzzZV4cTIBwr4kqFUv2Xa3LTS1WBBTOIRprI11egX4h8BSVUMwM-ehtsT02dDkmpWXZ1WfFEjC5SY2UcbXU_S6yCebvx_OqsfxhbrQ9Y7AUKJh-Z1mvYQZWXhTHLa9SNDV0aWYAEzI_inLhilIU7sDIqgHdmxLo8m-1pC9zBE7puoKYLO3xoxIAhFfRK_oYkdz3TR7VfAVmj_m58WB1mocGSM2Fpa6tjUfCrM_NTTF-fq85YRoeJYcYsUNgT_MJsOIi6rvHtjuKLsUBI42qZyMG1lPBG6NBIhJLOi70wYemKWuHwwV-CY6gbO5s5VQ6gdmd6Vcr1ekHj8CJfIP6d_H-mLpo6nqJNlpdkzj8Ezt6VxFHGrudzkNG-aDrA0QchjSPoMrdBXE1ne68GTZ5d2d9OFl3ISPanFQRM_EFedmnVLKlQX5uvkHaFOKTzakimsdFqAzJNg1aSkEJmco0LrRxZgzbx_6iV2HZTFHd3ltq5BNxoQbPD3JWSMihZ7V6BmCvh-wtHkH0QKJmgRUZ3k0i9BtTQ5Xxcq-NM-AHT0CdMsC0QkH8qPiFPhxCJEsGzJf87r5gRYywejd_JYZWvBLRQAEDV81eynjJU0ExY85vCtuk9vqYNjtfdvCLrxBjMj3FrwCVIDISkqFem1C1MxC7hCbcq1th90DZj6Ix-5qSmXQMhp-3nto_ecdb4MZ4Vw4nyBjlIhkQNFwc31JU9aDsKhzxEztZSOUrJT-A9yuSRAipuZ1aHR9dhVIFr78jCsKA97N8tUZQOFtZa8n5v2dhsygynMt-Z1quKMrkvrqeHxzOhxYhdag09PrwTzJtsYXUcZjCHRTCAa-TdQkJdMI7O280u0RDjd3_pg_B3RTBDrHjpXp_39mFhmmnF35mbOqiAJ28UCvPVEL5BfUxMlr99t38o9-3iMn4BCzftxq-nIz2gq9rODbIdyR6eXiofZ4G5GCkWViBtFZWcDbYH0r-XlP8_N3RvtQIWbjAPHBR-mSYHcDABo2Afv5M2QCRz_yrWgFeMAws6q-i7gieImgENhfxvNCo7o5A-UyXmhVTUMNROaR4sjDBwvmZEAsws3uMLZc2UGwJkvlpfUGj2LgPC0kBTF5R_t4BAwCXVhIp1uvoSttT3b5ZVvXDFrWybM8Uf5ClWuWo89Xtry6hZA3fHWcS8KQk0gcN1_jnQ=s1024" className="largePortrait br4" alt=""/>
+                <img src={projectImages?.bakeryCategoryImage} className="largePortrait br4" alt="" />
             </div>
 
             <div className="layoutDivision pr1 flexColumn">
@@ -14,7 +17,7 @@ const GalleryComponent = () => {
                 <div className="layoutPortraitDivision pb1">
 
                     <div className="quarter pr1">
-                        <img src="https://d1tf573zhz3zzy.cloudfront.net/data/content/images/itemImages/HOTEL/portrait/7095934f-61d6-40fd-93a2-89993605a8ae_twox_256x368.jpg" alt="" className="br4" />
+                        <img src={projectImages?.babyCareImage} className="br4" />
                     </div>
 
                     <div className="quarter pl1">
@@ -24,7 +27,7 @@ const GalleryComponent = () => {
                 </div>
 
                 <div className="layoutLandscapeDivision">
-                    <img src="https://lh3.googleusercontent.com/rd-gg-dl/AJfQ9KTEi3d4XWiP_AK7xDpTOFbA5O9XDXS_kJoslp1iEWDPMBeJg-HlEeCi1wkWrsyCspDTN7LTYiIA-ZYyG_LQJiwddVzQFm1WFI6tk3zH_vTEfrn3XAD3yjrGudDu9kxV8x3Hy5jxNuR5GffhkR6TZC3IvHsmzCDZZQV8-lGoEQWsjph-VxlBORWc4Vsc7XZLLdBFJayzo1Xe80WnW3H8lmNluIMeJKmR2M0QUg44QF58JrtM1Fd30Wtc9Zs3ftakaGZPzTa8S8Bf4c47LF80H8InkHCDgeivU7k53TJ_qAkoxm-2Uh4tcWpz2wTKaIpfO80QD4kCrOcTcwGpW4IKbtt5SpPb0CLwCDGYHX_P9Igu7wLsZ3xJztlE40Ei6mGefYnyustRPKRZkd_1WSzDhvgYe4e7cjEqzyqWpnk37JTUj2EoDu_erydkD3JqTURvTdRJIwEuO_vx75maUZBEPxMO-EpdWRQd39-y1FLlXwlnE4cUTHYzReD_m1PYC0PM-9ct0d2voeihGb6hHiv_PWzw-1vLd6uT-hG_sXlZGdfJQQkSOupEPPQOixWB4fsJGD0yAPCZE5V46urdGRJaVDcErRBB5SBEUGzTMcptWtkS0saso6JjA-VdNsVs7kpqe4CgkkktQPg_Qi_wBSuLaq-pOq8fT1_o5IlvYczj7a8VCa0eZvaya_PbvmaDuu7Hb6pmW0nGI0w2O7UYnX17BTyrFrRh0hq3LjYf19Q9v361cLkBM6na5gzts2oUmsYPyh4kvpzXZAKmQEie6K8w6Kcj2jInzQEegDd129-lsQYN9cETgnwCpzlLDTcftXhiesbWFS5zdRktq48hypKMGqH6BiGRzl6SexkgCz_ebu7DjP1nyEJsnk3vC9USX5RAQDo7BY8wr4lmh0ZgmodEFy-ClaLfKnMbuF1C-8Xqmftt_msCbDxjTj30VfZZeM1WtEcmhNyZc2jJnheu6XYalOKV_axR4SZRCaf5MGtiCRypFKiXrkmFEOsksTqM9KyqvpP0TdjgYWHShHkGcMKo52-pFlXnAxgUyKY4VHFfCMHFNVkqmj0iKeQ9KRaUCWjnszQw17C0EYgovErYTo0keb-pGKj1cZY8KS-8b1lXE5PB4GhzPVqk8WwWicocUG8ybCSYsMhmc7Adb5D7JfW9i9WmQ_H46cB16_gJoXW0-v9MdKOXqPsKHuJw2Vue28yv_lFViCMooE9ZPyoJNI20X4awMJwJbJVkxj5V-njyH5zhHDvlZ4_KK5Djch7htZ0u8uMvhYnQUSuwD1mHUrg8juzlCbnOSVomQ2XVM-iH5Wx8P_dOBgkesSyxu05PN5sIQS2EQ9UgFb8=s1024" alt="" className="br4" />
+                    <img src={projectImages?.healthyFoodImage} alt="" className="br4" />
                 </div>
 
             </div>
@@ -32,13 +35,13 @@ const GalleryComponent = () => {
             <div className="layoutDivision pr1 flexColumn">
 
                 <div className="layoutLandscapeDivision pb1">
-                    <img src="" alt="" className="br4" />
+                    <img src={projectImages?.chickenMeatImage} alt="" className="br4" />
                 </div>
 
                 <div className="layoutPortraitDivision">
 
                     <div className="quarter pr1">
-                        <img src="https://lh3.googleusercontent.com/rd-gg-dl/AJfQ9KR3J3If2qljBGx8YIPONQG6d_ryZZVWeu58Y4ZzlcXt2vZpTIFidCmDcr5YPy1uhNIOMT6sPf71YpFnmsl1RE9qXTPC27qHvw3EZXjspLuk3WZNrdJMMYN5uat8aSRViJRQw8SlU_V5-YEaclaJdPJuUS7XyDuKwU4zVjhdsU2BeCxsyimYgCiEaPqqwafiRcBAilFfRVsqX4C2YPgmWUJ6D3xiDZOE8KZE0kjjl5rrK3pkGMQ49s3e_ZyKV2WD0-gtKORbDD2e5cnpwvpeXou31NNC_EwZq6Id_RkHa1SHHBlE_Gr4S-UCk49o8wPAfuFs1R6OPpiDidf7XHh1lrJ09BJHEG1yi4cdjSJPeE3ihFt8YcPFPE9cGPkh-wqCdrjsw-BKTF66dtciN1RgQHlVCGlUmDkCf_XIWuihuFjkw_YRDG0Xt8M8vIKdHtBywqdszvR1lmhpILqhnKXiERsLOQBMZC3aPD33z_U24fa9o_1apady1_tyobGS7bUydFTqU41-_rCcVLPyi_JXU6iDA5rb1xVJYyu6oVHwX30v79FgxEPdYMOduESzrIN5jmXrZ4-phQeP20jek9PLr1Nx540iZVG5GXl2VJ7tqfDny-HUe1v7_Z_DWDO0MkjxP_yhjvuONAUXGOAOZeYgYq3Rt3cKS3MLTWuwkmhXB47PsKmKqMwqNAQKfxTKr_pe08zYCYb2ZPuTeiarWFqPXlw3CA5HCHFFS6UdEfMukXD9JPrJPBjLWlTA_sfV0h6TwDjV-jt41j4YG7S24pBmui1YNgS8gWFKQ17-KshMNSWOy8jAn9ZkxuHRspB0EBu1Bltl0BFEo9ImRi0HnHZK8rVBDC9uJO0iy7CDw_Az1zmkUCYL3exO5s5f4pLxceCb5nZtdUtOddCQVU1-gMUJMLHFg8Qmjqd2aRxmIktMJlYOp9YE4CeT_f77B3UrQuZBh-8AlLJZNBRqzH9x9G9lM8DFgKQn_l7-sT9--9lrSfA_H5wmukKEawLIltwTVrzpHACV5W3DK8U6at3Ni3qISDwTiPPdnnRZhHxrF4iAW-ebnyzzea7lvKWST7BWgjYLpQNYMYn0KHpdJpSbtpOGgA-w8EjABQLQBFaYSdiHsiJM5MewvGIsbg1WI2HQDUfjWEiREmFjTzndNNkyYduTaZr9necSC6pptwdafSHTG6XFsp0_rKVfqR6xbIDvFAjNnchCVVG4fuhzq9yexIJ3ddPUVeevm3c9e7AsVfmgJX5hfXBNTyGqtLNt9sAR_hGKPKSsIQb6cpXWoLqx-aw51P4qHjkU83hlndidB3zFRAk8U0yalXmbZwrTbyxqKD7DHn4FiiPcF9Y=s1024" alt="" className="br4" />
+                        <img src="" alt="" className="br4" />
                     </div>
 
                     <div className="quarter pl1">
@@ -64,7 +67,7 @@ const GalleryComponent = () => {
                 </div>
 
                 <div className="layoutLandscapeDivision">
-                    <img src="https://lh3.googleusercontent.com/rd-gg-dl/AJfQ9KRtLIJsLjq8DYrXT4p0q8VOaGkmjCyzxZJpnsrFrswsm9WXxHo8hoqKVUqFuFGPWa7w9xZ7fybFNElxsaKBZLtEh0piMkHk5Rejr3QOwbAsQ8enwaHhChydzHkqqtF2rGQfnnKQa8npofJ2Gk9i6VIYpcmv8ejfp20AjnbGrJwwLETtRrAn-ChWDg-fnwXdGOpxIBRe9ko5yOzARReV_JnLYGAJqFoxnFX30VEVu6wYbXyqwtKsYkH8wrdrQ7_oFhxSJ05OjRL9mchzP1K2siph0eznv-KCgUeOCs_fZj_ICLqSUW8M_6o7IzpHJh6P5FJoaa1MiS1H_n5FEfdkJk-Gb-fY-9wsKmUPQKz3P9BrJ3paBHAUO7KiEKrN2dE4PcvnMgF2oBj0ZJYXtwJiJOoqlTeH2JnyIB5sgu44ZFnP3CqbUFAdfC4DQsKrrYZSgyWL9gsHQJCp0UGRHhp43oB3EsZudhJP-WnS2nkqiJcYSlVVd4jZyALFLDWFkObMSwk_txzZdnK8mlbwXmhPlZzlvmur6c7bRmGoqy-WmbY0RXns0ciDM5INQ9S0p2A0jgj7xWluZCIvBSj4cuVdfhHABDs1MUJFbpNJk9PzFDd1vIzGQXIbgBJWSEOhAR8eapStBae0HEnDSensVsD_dDJoJC4BMy1YJdCjRRt_I2USAKwFXlZB4Mfzh2eOD8uXw9EQryKKINHPFY6FXlvevkRDj4Ndh7jaH6Q9h2C1DHv7Zxy2b428z7fcNaIQxgaSvNzGyKsgie-GKWfN8jr86y5DrUtRDwigCzmi-E9p2PNWnzG-LkuJFpkdW1xFencLflmE68HvILta59MnmiK1LzHjorC2Eyov5bkD9ek6t1vUosMfUmtehRmZwVPYhGJbZHURHTR4E_NQyMvATTnUs90kkK7E9yQwQK7HR0nwPpQZl_eJS3-nxuWJkAYOs3swoKJVxW23zmAafJEremCclyohX2L8Ab4xYR9zjmCMGOiPmDqiLtW8n9bi4450IzpI_DXIz8Mt3vUYpuSRgRcQldODP7rqpByXabVCyDeRRgpq4Os60fLegL96ZKVWGx7eoNQY6JbIiyqATzwRHGSX1NBVR-lm5SQ3U4q9S_YSI5kIj0k4OeUyTigDANxGP10rdCRQaA6mojf2P6mDlpVEjmirR9gg6wHBHiNK4fbFl1nAN0jqDIGsCqd1vDvIgDHipdmc7GAQ6fBCobhRue4g3qr0ZRecUQauIBlU0s4CCZemmNkCxA0m8ErCMyrZDSFTV_d9teoi23Gc1yOe2Ns0A7eLOSakFXqS8_hg8WqEwjtQumxTCWQNjNAix0DUk84xooJ5HkywGQ=s1024" alt="" className="br4" />
+                    <img src={projectImages?.drinksImage} alt="" className="br4" />
                 </div>
 
             </div>
