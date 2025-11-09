2672f2ed6c108a8899b25f7cc024ddec20480ad7
DeepanshUnravel <deepansh@gounravel.com>
2025-11-10 01:49:27 +0530

    Add CategoryCard component, update Category page layout, and enhance styles for better UI

--- client/src/Pages/Account/Category.jsx
+++ client/src/Pages/Account/Category.jsx
@@ -4,33 +4,52 @@ import PageBanner from "../../Common/PageBanner";
 import AddCategoryModal from "../../Components/AddCategoryModal.jsx";
 import { APIConfig } from "../../Networking/Configuration/ApiConfig.js";
 import { getApiRequestWrapper } from "../../Networking/Services/ApiCalls.js";
+import CategoryCard from "../../Common/CategoryCard.jsx";
 
 const Category = () => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [categoryList, setCategoryList] = useState(null);
+    const [isUploaded, setIsUploaded] = useState(false);
 
     function handleModalClose() {
         return setIsModalOpen(!isModalOpen);
     };
 
     async function fetchCategory() {
-        const CATEGORY_URL = APIConfig?.categoryApiPath?.getAllCategory;
-        const response = await getApiRequestWrapper(CATEGORY_URL);
+        try {
+            const CATEGORY_URL = APIConfig?.categoryApiPath?.getAllCategory;
+            const response = await getApiRequestWrapper(CATEGORY_URL);
 
-        console.log(response);
+            if (response && response?.success === true && response?.error === false) {
+                setCategoryList(response?.categoryData);
+            }
 
-    }
+        } catch (error) {
+            console.log(error.message);
+
+        };
+    };
 
     useEffect(() => {
         fetchCategory()
-    }, [])
+    }, [isUploaded])
+
+    console.log(categoryList);
+
 
     return (
-        <Box sx={{
-            position: "relative"
-        }}>
+        <section className="category-page">
 
             <PageBanner heading={"category"} closeModal={handleModalClose} isModalOpen={isModalOpen} />
+
+            <aside className="add-category-button">
+                {!isModalOpen && (
+
+                    <button className="imageUploadButton" onClick={handleModalClose} >+ Add Category</button>
+
+                )}
+            </aside>
 
             <Container
                 disableGutters
@@ -44,10 +63,18 @@ const Category = () => {
                     position: "absolute",
                 }}
             >
-                {isModalOpen && <AddCategoryModal closeModal={handleModalClose} />}
+                {isModalOpen && <AddCategoryModal closeModal={handleModalClose} setIsUploaded={setIsUploaded} />}
             </Container>
 
-        </Box>
+            <div className="display-category-container">
+
+                {categoryList?.map((categoryItem, index) => {
+                    return <CategoryCard {...categoryItem} key={index} />
+                })}
+
+            </div>
+
+        </section>
     );
 };
 
