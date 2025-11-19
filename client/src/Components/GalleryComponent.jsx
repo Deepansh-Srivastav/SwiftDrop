b2f97c1e202e69f5d00840aad265a026b371fa5f
DeepanshUnravel <deepansh@gounravel.com>
2025-11-20 02:44:47 +0530

    Add loading skeletons to GalleryComponent for improved user experience during image loading

--- client/src/Components/GalleryComponent.jsx
+++ client/src/Components/GalleryComponent.jsx
@@ -1,15 +1,17 @@
-
-import "../Styles/Gallery.css"
-import {
-    projectImages
-} from "../Assets/Assets.js"
+import "../Styles/Gallery.css";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const GalleryComponent = ({ preview }) => {
     return (
         <section className="mainContainer">
 
             <div className="layoutDivision pr1 ">
-                <img src={preview?.[0]?.secondaryImage} className="largePortrait br4" alt="" />
+                {preview?.[0]?.secondaryImage ? (
+                    <img src={preview[0].secondaryImage} className="largePortrait br4" alt="" />
+                ) : (
+                    <Skeleton height={350} className="br4 largePortrait" />
+                )}
             </div>
 
             <div className="layoutDivision pr1 flexColumn">
@@ -17,17 +19,29 @@ const GalleryComponent = ({ preview }) => {
                 <div className="layoutPortraitDivision pb1">
 
                     <div className="quarter pr1">
-                        <img src={preview?.[1]?.secondaryImage} className="br4" alt="" />
+                        {preview?.[1]?.secondaryImage ? (
+                            <img src={preview[1].secondaryImage} className="br4" alt="" />
+                        ) : (
+                            <Skeleton height={160} className="br4" />
+                        )}
                     </div>
 
                     <div className="quarter pl1">
-                        <img src={preview?.[2]?.secondaryImage} className="br4" alt="" />
+                        {preview?.[2]?.secondaryImage ? (
+                            <img src={preview[2].secondaryImage} className="br4" alt="" />
+                        ) : (
+                            <Skeleton height={160} className="br4" />
+                        )}
                     </div>
 
                 </div>
 
                 <div className="layoutLandscapeDivision border-shadow">
-                    <img src={preview?.[3]?.secondaryImage} alt="" className="br4" />
+                    {preview?.[3]?.secondaryImage ? (
+                        <img src={preview[3].secondaryImage} alt="" className="br4" />
+                    ) : (
+                        <Skeleton height={180} className="br4" />
+                    )}
                 </div>
 
             </div>
@@ -35,17 +49,29 @@ const GalleryComponent = ({ preview }) => {
             <div className="layoutDivision pr1 flexColumn">
 
                 <div className="layoutLandscapeDivision pb1">
-                    <img src={preview?.[4]?.secondaryImage} alt="" className="br4" />
+                    {preview?.[4]?.secondaryImage ? (
+                        <img src={preview[4].secondaryImage} alt="" className="br4" />
+                    ) : (
+                        <Skeleton height={180} className="br4" />
+                    )}
                 </div>
 
                 <div className="layoutPortraitDivision">
 
                     <div className="quarter pr1">
-                        <img src={preview?.[5]?.secondaryImage} alt="" className="br4" />
+                        {preview?.[5]?.secondaryImage ? (
+                            <img src={preview[5].secondaryImage} alt="" className="br4" />
+                        ) : (
+                            <Skeleton height={160} className="br4" />
+                        )}
                     </div>
 
                     <div className="quarter pl1">
-                        <img src={preview?.[6]?.secondaryImage} alt="" className="br4" />
+                        {preview?.[6]?.secondaryImage ? (
+                            <img src={preview[6].secondaryImage} alt="" className="br4" />
+                        ) : (
+                            <Skeleton height={160} className="br4" />
+                        )}
                     </div>
 
                 </div>
@@ -57,17 +83,29 @@ const GalleryComponent = ({ preview }) => {
                 <div className="layoutPortraitDivision pb1">
 
                     <div className="quarter pr1">
-                        <img src={preview?.[7]?.secondaryImage} alt="" className="br4" />
+                        {preview?.[7]?.secondaryImage ? (
+                            <img src={preview[7].secondaryImage} alt="" className="br4" />
+                        ) : (
+                            <Skeleton height={160} className="br4" />
+                        )}
                     </div>
 
                     <div className="quarter">
-                        <img src={preview?.[8]?.secondaryImage} alt="" className="br4" />
+                        {preview?.[8]?.secondaryImage ? (
+                            <img src={preview[8].secondaryImage} alt="" className="br4" />
+                        ) : (
+                            <Skeleton height={160} className="br4" />
+                        )}
                     </div>
 
                 </div>
 
                 <div className="layoutLandscapeDivision">
-                    <img src={preview?.[9]?.secondaryImage} alt="" className="br4" />
+                    {preview?.[9]?.secondaryImage ? (
+                        <img src={preview[9].secondaryImage} alt="" className="br4" />
+                    ) : (
+                        <Skeleton height={180} className="br4" />
+                    )}
                 </div>
 
             </div>
