e29dc16fd94c655f799eb74479b791c57c935cc1
DeepanshUnravel <deepansh@gounravel.com>
2025-12-04 02:22:40 +0530

    changed the style in the carts page

--- client/src/Pages/Cart.jsx
+++ client/src/Pages/Cart.jsx
@@ -96,7 +96,7 @@ const Cart = () => {
                     <div className="cart-data-container hide-scroll-bar">
 
                         <div className="cart-products-container hide-scroll-bar">
-                            
+
                             <div className="products-container">
                                 <Divider sx={{ width: "100%" }} />
 
@@ -104,7 +104,7 @@ const Cart = () => {
                                     return (
                                         <>
                                             <CartProductCard {...product} key={index} />
-                                            <Divider sx={{ width: "100%",color:"black" }} />
+                                            <Divider sx={{ width: "100%", color: "black" }} />
                                         </>
                                     )
                                 })}
@@ -187,7 +187,40 @@ function CartProductCard({ discount, finalPrice, name, price, productId, quantit
     return (
         <article className="cart-product-card-wrapper">
 
-            <div className="cart-product-card-info-container">
+            <div className="cart-product-info">
+
+                <div className="product-card-info-container">
+                    <p className='text-size-3 product-name'>{name}</p>
+                    <div style={{display:"flex",gap:"0.5rem"}}>
+                        <p className='unit-badge'>{unit}</p>
+                        <p className='unit-badge'>Discount - {discount}%</p>
+                    </div>
+                </div>
+
+
+                <div className="product-card-footer">
+
+
+                    <div className="price-container">
+                        <span className='price'>₹{price}</span>
+                        <span className='final-price'>₹{finalPrice}</span>
+                    </div>
+
+                </div>
+
+                <div className="quantity-counter margin-top-20">
+                    <button className="qty-btn" onClick={() => { handleQuantity("sub") }}>-</button>
+                    <span className="qty-value">{itemQuantity}</span>
+                    <button className="qty-btn" onClick={() => { handleQuantity("add") }}>+</button>
+                </div>
+
+            </div>
+
+            <div className="cart-product-image">
+                <img src={image[0]} alt="" />
+            </div>
+
+            {/* <div className="cart-product-card-info-container">
                 <p className="text-size-custom bold" style={{ textOverflow: "ellipsis" }}>{name}</p>
                 <div className="price-container">
                     <span className='price'>₹{price}</span>
@@ -205,7 +238,7 @@ function CartProductCard({ discount, finalPrice, name, price, productId, quantit
 
             <div className="cart-product-card-image-container">
                 <img src={image[0]} alt="" />
-            </div>
+            </div> */}
 
         </article>
     )
